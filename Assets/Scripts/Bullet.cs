f108ea004ae930d8db7dcf1293e7b8d95e86cc96
Patrick Haikal <pathaikal@gmail.com>
2017-01-21 00:29:27 +1100

    Added simple player movement

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	public Vector2 direction;
+
+	[SerializeField]
+	private int movementSpeed;
+	[SerializeField]
+	private int range;
+	private Vector2 startPosition;
+
+	void Start()
+	{
+		startPosition = this.transform.position;
+	}
+
+	void Update()
+	{
+		Vector2 position = this.transform.position;
+		position.x += direction.x * movementSpeed * Time.deltaTime;
+		position.y += direction.y * movementSpeed * Time.deltaTime;
+		this.transform.position = position;
+
+		if(Vector2.Distance(this.transform.position, startPosition) > range)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	public GameObject bulletPrefab;
+
+	[SerializeField]
+	private int movementSpeed;
+	private Vector2 direction;
+	[SerializeField]
+	private int dodgeDistance;
+	[SerializeField]
+	private float dodgeCooldown;
+	private float lastDodgeTime;
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.W))
+		{
+			direction.y += 1;
+		}
+		if(Input.GetKeyDown(KeyCode.S))
+		{
+			direction.y -= 1;
+		}
+		if(Input.GetKeyDown(KeyCode.A))
+		{
+			direction.x -= 1;
+		}
+		if(Input.GetKeyDown(KeyCode.D))
+		{
+			direction.x += 1;
+		}
+
+		if(Input.GetKeyUp(KeyCode.W))
+		{
+			direction.y -= 1;
+		}
+		if(Input.GetKeyUp(KeyCode.S))
+		{
+			direction.y += 1;
+		}
+		if(Input.GetKeyUp(KeyCode.A))
+		{
+			direction.x += 1;
+		}
+		if(Input.GetKeyUp(KeyCode.D))
+		{
+			direction.x -= 1;
+		}
+
+		Move();
+
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Dodge();
+		}
+
+		if(Input.GetKeyDown(KeyCode.E))
+		{
+			Shoot();
+		}
+	}
+
+	private void Move()
+	{
+		if(direction.x != 0 || direction.y != 0)
+		{
+			Vector2 position = this.transform.position;
+			position.x += direction.x * movementSpeed * Time.deltaTime;
+			position.y += direction.y * movementSpeed * Time.deltaTime;
+			this.transform.position = position;
+		}
+	}
+
+	private void Dodge()
+	{
+		if(Time.time - lastDodgeTime > dodgeCooldown && (direction.x != 0 || direction.y != 0))
+		{
+			Vector2 position = this.transform.position;
+			position.x += direction.x * dodgeDistance;
+			position.y += direction.y * dodgeDistance;
+			this.transform.position = position;
+
+			lastDodgeTime = Time.time;
+		}
+	}
+
+	private void Shoot()
+	{
+		if(direction.x != 0 || direction.y != 0)
+		{
+			GameObject bullet = Instantiate(bulletPrefab, this.transform.position, Quaternion.identity);
+			bullet.GetComponent<Bullet>().direction = direction.normalized;
+		}
+	}
+}
