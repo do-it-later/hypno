ebdc04fa59610357a11b6b0bdcc63e60ff7eb28e
Patrick Haikal <pathaikal@gmail.com>
2017-01-21 12:47:45 +1100

    Added some background music

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager instance = null;
+
+	public AudioClip menuMusic;
+	public AudioClip gameStartMusic;
+	public AudioClip gameBuildupMusic;
+	public AudioClip gameEndingLoopMusic;
+
+	void Awake()
+	{
+		if(instance == null)
+			instance = this;
+		else if(instance != this)
+			Destroy(gameObject);
+
+		DontDestroyOnLoad(gameObject);
+	}
+
+	void Start()
+	{
+		SoundManager.instance.PlayLoopedMusic(menuMusic);
+	}
+
+	void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.P))
+		{
+			StartCoroutine(PlayGameMusic());
+		}
+	}
+
+	IEnumerator PlayGameMusic()
+	{
+		SoundManager.instance.PlaySingleMusic(gameStartMusic);
+		yield return new WaitForSeconds(gameStartMusic.length);
+		SoundManager.instance.PlaySingleMusic(gameBuildupMusic);
+		yield return new WaitForSeconds(gameBuildupMusic.length);
+		SoundManager.instance.PlayLoopedMusic(gameEndingLoopMusic);
+	}
+}
