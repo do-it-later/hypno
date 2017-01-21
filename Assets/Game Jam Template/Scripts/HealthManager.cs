2d8c2b13a5270ba42e8e72faef1176636ec283f7
jpinto <jarrydpinto@gmail.com>
2017-01-21 15:40:53 +1100

    Added charge bar. Made update function generic.

--- Assets/Game Jam Template/Scripts/HealthManager.cs
+++ Assets/Game Jam Template/Scripts/HealthManager.cs
@@ -5,10 +5,17 @@ using UnityEngine.UI;
 
 public class HealthManager : MonoBehaviour
 {
-    public Image healthBar;
+    public Image[] healthBars;
+    public Image[] chargeBars;
+    public Image element;
+    public enum UIObject
+    {
+        HEALTH,
+        CHARGE
+    };
 
     // TODO: For testing. Delete or comment out! - JP
-    float health = 100;
+    //float health = 100;
 
 	// Use this for initialization
 	void Start ()
@@ -18,15 +25,33 @@ public class HealthManager : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        --health;
-        updateHealth(0, health);
+        // TODO: For testing. Delete or comment out! - JP
+        //if (health > 0)
+        //{
+        //    --health;
+        //    updateUIItem(1, UIObject.HEALTH, health);
+        //    updateUIItem(1, UIObject.CHARGE, health);
+        //}
 	}
 
-    public void updateHealth(int playerId, float health)
+    // playerId: 1 to number of players    objectType: HEALTH/CHARGE    percentage: 0-100
+    public void updateUIItem(int playerId, UIObject objectType, float percentage)
     {
-        if (health >= 0 && health <= 100)
+        if (playerId-1 < 0 || playerId-1 > 2 || percentage < 0 || percentage > 100)
         {
-            healthBar.rectTransform.localScale = new Vector3(health/100, healthBar.rectTransform.localScale.y, healthBar.rectTransform.localScale.z);
+            return;
         }
+
+        Image element = null;
+        if (objectType == UIObject.HEALTH)
+        {
+            element = healthBars[playerId - 1];
+        }
+        else if (objectType == UIObject.CHARGE)
+        {
+            element = chargeBars[playerId - 1];
+        }
+
+        element.rectTransform.localScale = new Vector3(percentage / 100, element.rectTransform.localScale.y, element.rectTransform.localScale.z);
     }
 }
