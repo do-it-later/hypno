8d7d01d2fb1de14164ef06eee65c92d4bc8f4377
jpinto <jarrydpinto@gmail.com>
2017-01-22 12:58:47 +1100

    Added game over screen. Added placeholder player status elements.

---
+++ Assets/Scripts/UICooldown.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UICooldown : MonoBehaviour
+{
+	[SerializeField]
+	private Image dashImage;
+    
+    // ENSURE THAT VALUE IS BETWEEN 0 AND 1!
+	public void UpdateDashCooldown(float value)
+	{
+        dashImage.fillAmount = 1 - value;
+	}
+}
